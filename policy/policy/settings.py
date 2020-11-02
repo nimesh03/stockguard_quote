78529afe087a988a2426ecf77e728122e0d209b6
Nimesh <nimesh19@hotmail.ca>
2020-11-01 23:04:52 -0500

    change

--- policy/policy/settings.py
+++ policy/policy/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
